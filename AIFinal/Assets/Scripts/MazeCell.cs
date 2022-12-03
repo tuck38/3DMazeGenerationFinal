b33960a014aca0ce4ec120fe2db7945960e706bb
Thomas Wagner <thomas.wagner@mymail.champlain.edu>
2022-12-03 16:24:27 -0500

    added script to prefab

--- AIFinal/Assets/Scripts/MazeCell.cs
+++ AIFinal/Assets/Scripts/MazeCell.cs
@@ -7,5 +7,7 @@ public class MazeCell : MonoBehaviour
     bool north, south, east, west; //walls
     int set; //which set the cell belongs to
 
+    [SerializeField] GameObject northWall, eastWall, southWall, westWall;
+
 
 }
