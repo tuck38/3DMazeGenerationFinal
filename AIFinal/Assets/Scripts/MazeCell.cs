fc35f311dd16eb10a1c8f642060464ebcb91d36d
Thomas Wagner <thomas.wagner@mymail.champlain.edu>
2022-12-03 17:16:22 -0500

    cell

--- AIFinal/Assets/Scripts/MazeCell.cs
+++ AIFinal/Assets/Scripts/MazeCell.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class MazeCell : MonoBehaviour
 {
-    bool north, south, east, west; //walls
     int set; //which set the cell belongs to
 
     [SerializeField] GameObject northWall, eastWall, southWall, westWall;
+
+
+    public void SetNorth(bool value) { northWall.SetActive(value); }
+    public void SetSouth(bool value) { southWall.SetActive(value); }
+    public void SetEast(bool value) { eastWall.SetActive(value); }
+    public void SetWest(bool value) { westWall.SetActive(value); }
+
+    public bool GetNorth() { return northWall.activeSelf; }
+    public bool GetSouth() { return southWall.activeSelf; }
+    public bool GetEast() { return eastWall.activeSelf; }
+    public bool GetWest() { return westWall.activeSelf; }
+
+
+
+
 
 
 }
